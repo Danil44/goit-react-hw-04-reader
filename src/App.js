d9fb74064af75b4e996d8714668fc42a7d4505c2
Daniil Golovenko (CODER) <d.golovenko@itcg.ua>
2019-06-08 23:46:49 +0300

    remove home route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { lazy, Suspense } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 import { Redirect } from 'react-router';
 
 const ReaderPage = lazy(() => import('./pages/ReaderPage'));
 
-const Home = lazy(() => import('./pages/Home'));
-
 const App = () => {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/reader" component={ReaderPage} />
-          <Redirect to="/reader" />
-        </Switch>
-      </Suspense>
+      <HashRouter basename="/">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/reader" component={ReaderPage} />
+            <Redirect to="/reader" />
+          </Switch>
+        </Suspense>
+      </HashRouter>
     </div>
   );
 };
