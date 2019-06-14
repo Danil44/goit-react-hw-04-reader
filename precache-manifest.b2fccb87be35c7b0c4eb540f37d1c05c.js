24195e77398eb3fe3844b63bb6fcb2d84b8c7b06
Daniil Golovenko (CODER) <d.golovenko@itcg.ua>
2019-06-14 20:47:20 +0300

    Updates

--- precache-manifest.b2fccb87be35c7b0c4eb540f37d1c05c.js
+++ precache-manifest.b2fccb87be35c7b0c4eb540f37d1c05c.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "80f2903fd59f91382d12",
-    "url": "/goit-react-hw-04-reader/static/js/runtime~main.fb39e2d0.js"
+    "revision": "01ca9a51284e29ea0e33",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.fe870612.js"
   },
   {
     "revision": "8847a36faad5711cbbae",
     "url": "/goit-react-hw-04-reader/static/js/main.066206ae.chunk.js"
   },
   {
-    "revision": "d8ba7c8a02814d78469f",
-    "url": "/goit-react-hw-04-reader/static/js/3.822ddd41.chunk.js"
+    "revision": "4495592ee354708296b5",
+    "url": "/goit-react-hw-04-reader/static/js/3.d28efa9f.chunk.js"
   },
   {
     "revision": "56ef636fe6798ee8a79b",
     "url": "/goit-react-hw-04-reader/static/js/2.2b8b16a9.chunk.js"
   },
   {
-    "revision": "d8ba7c8a02814d78469f",
+    "revision": "4495592ee354708296b5",
     "url": "/goit-react-hw-04-reader/static/css/3.9c2676f3.chunk.css"
   },
   {
-    "revision": "b9aea0d1df3690b2a8d60a59b559817b",
+    "revision": "dc5763cbd33794f97c25baed9aa92e03",
     "url": "/goit-react-hw-04-reader/index.html"
   }
 ];
